e56dd380feef3bf1423c6a04cf56eb6d1e441d65
Alvaro <alvaro9943310@gmail.com>
2024-07-12 04:03:13 -0300

    primer commit

---
+++ src/main/java/com/agencia/sidpla/SidplaApplication.java
@@ -0,0 +1,13 @@
+package com.agencia.sidpla;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SidplaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SidplaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/agencia/sidpla/SidplaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.agencia.sidpla;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SidplaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
